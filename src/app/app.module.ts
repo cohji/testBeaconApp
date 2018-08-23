f2b559acb541e216b8dfa6bcad3ae09a66723106
cohji <koji.suzuki.cc@gmail.com>
2018-08-23 21:51:36 +0900

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { IBeacon } from "@ionic-native/ibeacon";
+import { LocalNotifications } from '@ionic-native/local-notifications';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -34,6 +37,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    IBeacon,
+    LocalNotifications,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,78 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import { IBeacon } from '@ionic-native/ibeacon';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  private uuid: string = 'B9407F30-F5F8-466E-AFF9-25556B57FE6D';
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private localNotifications: LocalNotifications, private readonly platform: Platform, private readonly ibeacon: IBeacon) {
 
+    this.platform.ready().then(async () => {
+      console.log('async platform.ready()');
+    });
+
+    this.platform.ready().then(() => {
+      console.log('platform.ready()');
+    });
+  }
+    
+  ngOnInit() {
+    this.platform.ready().then(() => {
+      console.log('ngOnInit');
+      this.startBleFun();
+    });
   }
 
+  
+  public startBleFun(): void {
+
+
+    // Request permission to use location on iOS
+    this.ibeacon.requestAlwaysAuthorization();
+    // create a new delegate and register it with the native layer
+    let delegate = this.ibeacon.Delegate();
+
+    // Subscribe to some of the delegate's event handlers
+    // delegate.didRangeBeaconsInRegion().subscribe(
+    //   (pluginResult: IBeaconPluginResult) => console.log('didRangeBeaconsInRegion: ', pluginResult),
+    //   (error: any) => console.error(`Failure during ranging: `, error)
+    // );
+    delegate.didStartMonitoringForRegion().subscribe(
+      pluginResult => console.log('didStartMonitoringForRegion: ', pluginResult),
+      (error: any) => console.error('Failure during starting of monitoring: ', error)
+    );
+
+    delegate.didEnterRegion().subscribe(
+      pluginResult => {
+        console.log('didEnterRegion: ', pluginResult);
+        this.localNotifications.schedule({
+          id: 1,
+          title: "didEnterRegion",
+          text: "pluginResult",
+          trigger: {at: new Date(new Date().getTime() + (1 * 1000))}
+        });
+      }
+    );
+
+    console.log('Creating BeaconRegion with UUID of: ', this.uuid);
+    const beaconRegion = this.ibeacon.BeaconRegion('nullBeaconRegion', this.uuid, 0, 0);
+
+    this.ibeacon.startMonitoringForRegion(beaconRegion).then(
+      () => console.log('Native layer recieved the request to monitoring'),
+      (error: any) => console.error('Native layer failed to begin monitoring: ', error)
+    );
+
+    // this.ibeacon.startRangingBeaconsInRegion(beaconRegion)
+    //   .then(() => {
+    //     console.log(`Started ranging beacon region: `, beaconRegion);
+    //   })
+    //   .catch((error: any) => {
+    //     console.error(`Failed to start ranging beacon region: `, beaconRegion);
+    //   });
+  }
 }
